1e221f1be22f6fce2c2ced8bc9a80ee5dba7358d
Dmitry Khodakovsky <dmitry.khodakovksy@epam.com>
2019-03-29 21:39:48 +0300

    added broken test

--- test-auto-sample/src/test/java/ru/levelup/test/auto/sample/SampleTest.java
+++ test-auto-sample/src/test/java/ru/levelup/test/auto/sample/SampleTest.java
@@ -79,6 +79,38 @@ public class SampleTest {
         driver.close();
     }
 
+    @Test
+    public void loginTestDuplicateBroken() {
+        // Init driver
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        // Open page
+        driver.get("https://epam.github.io/JDI/index.html");
+
+        // check title
+        assertEquals("Home Page", driver.getTitle());
+
+        // click user icon
+        driver.findElement(By.id("user-icon")).click();
+
+        // login
+        WebElement userNameTextField = null; //= driver.findElement(By.cssSelector("#name"));
+        userNameTextField.sendKeys("epam");
+        WebElement passwordTextField = driver.findElement(By.xpath("//input[@id='password']"));
+        passwordTextField.sendKeys("1234");
+        WebElement enterButton = driver.findElement(By.id("login-button"));
+        enterButton.click();
+
+        // Check user name
+        assertEquals("Piter Chailovskii1".toUpperCase(), driver.findElement(By.id("user-name")).getText());
+
+        // actions
+
+        // close browser
+        driver.close();
+    }
+
 
     //*[@id="wd-_stream_now"]//span[@class='streamnow__item-title']
     //*[@id="wd-_stream_now"]//ul[contains(@class,'streamnow__items')]//span[@class='streamnow__item-title']
